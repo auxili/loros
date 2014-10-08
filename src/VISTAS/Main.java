42d81a9697e9a129b869de86a5b66d84da358b26
kike <>
2014-10-08 13:23:41 +0200

    init

---
+++ src/VISTAS/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package VISTAS;
+
+import NEGOCIO.Loro;
+
+/**
+ *
+ * @author Papi
+ */
+public class Main {
+    public static void main (String[]args){
+        Loro loro = new Loro();
+    }
+    
+}
